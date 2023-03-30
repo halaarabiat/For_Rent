4bc0d10a942dc6f375eae0679482462c85c95b50
tala <tala.arabiat123@gmail.com>
2023-03-30 23:33:14 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,18 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:rent/welcome_screen.dart';
-import 'package:rent/forget_pass_screen.dart';
-import 'package:rent/signin_screen.dart';
-import 'package:rent/login_screen.dart';
-
 
 void main() { runApp(const MyApp());}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorSchemeSeed: const Color(0xff79698e),
+    return const MaterialApp(
 
-        //primarySwatch: Colors.blue,
-      ),
-      routes:{
-        "/SignInScreen":(context)=>const SignInScreen(),
-        "/LogInScreen":(context)=>const LogInScreen(),
-        "/ForgetPassScreen":(context)=>const ForgetPassScreen(),
-        "/WelcomeScreen":(context)=>const BottomSheetApp(),
-      },
-      initialRoute: '/WelcomeScreen',
-      //home: const ForgetPassScreen(),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -43,7 +27,7 @@ class SplashScreenState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3),
+    Timer(const Duration(seconds: 4),
             ()=>Navigator.pushReplacement(context,
             MaterialPageRoute(builder:
                 (context) => const BottomSheetApp()
